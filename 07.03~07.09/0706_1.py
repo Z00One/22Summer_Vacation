3abc9de1c711abc47afd2b3f898ca76e862fe664
Z00One <102473964+Z00One@users.noreply.github.com>
2022-07-06 12:55:24 +0900

    Create 0706_1.py

---
+++ 07.03~07.09/0706_1.py
@@ -0,0 +1,14 @@
+# while 문을 종료하는 방법
+
+# 방법 1 --> break
+# while True:
+#     num = int(input("정수 입력"))
+
+#     if num == 1:
+#         # 탈출
+#         break
+
+#  변수나 조건식을 사용
+import gugu as gu
+
+gu.gugudan()
