0ce2fccba7fb60d55b1469c96b063ccce2696304
Z00One <102473964+Z00One@users.noreply.github.com>
2022-07-07 18:44:09 +0900

    Update list0.py

--- 07.03~07.09/list0.py
+++ 07.03~07.09/list0.py
@@ -1,5 +1,8 @@
 mystring = "#heelo!"
+import random as r
 
 mylist = [element for element in mystring if element.islower() or element != "#"]
 
 print(mylist)
+List = [r.randint(1,20) for element in range(20) ]
+print(List)
