9dac467cc1695b71de9890c49791538ad9b6d967
Z00One <102473964+Z00One@users.noreply.github.com>
2022-07-08 12:51:33 +0900

    Update list1.py

--- 07.03~07.09/list1.py
+++ 07.03~07.09/list1.py
@@ -1,9 +1,14 @@
+import random
 last_value = int(input("수를 입력해요"))
-list1 = [value for value in range(1,last_value+1)] # [1, 2, 3, 4, ... last_value]
+list1 = [random.randint(1,20) for value in range(1,last_value+1)] # [1, 2, 3, 4, ... last_value]
 
-list2 = []
-index = 0
-while len(list2) < len(list1):
-    list.append(list1[index]) # 오류남
-    print(list2[0:])
-    index += 1
+# list2 = []
+# index = 0
+# while len(list2) < len(list1):
+#     list.append(list1[index]) # 오류남
+#     print(list2[0:])
+#     index += 1
+sum = 0
+print(list1)
+a = [ele for ele in list1 if ele >5 and ele <= 10]
+print((a))
