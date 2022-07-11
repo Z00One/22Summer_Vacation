0eaca1151ad4136bf77ce9d8fbe88d45e95d5273
Z00One <102473964+Z00One@users.noreply.github.com>
2022-07-11 11:37:04 +0900

    Update 과제_랜덤함수_2201303_3.py

--- 07.03~07.09/과제_랜덤함수_2201303_3.py
+++ 07.03~07.09/과제_랜덤함수_2201303_3.py
@@ -1,19 +1,22 @@
-import random as r
+import random
 
 # sort 함수 정의
-def sort(list_value, min_value, max_value):
-    new_list = []
-    for index_value in range(min_value, max_value + 1):
-        # 제일 작은 수부터 제일 큰 수까지 입력된 리스트에 있다면
-        while index_value in list_value:
-            # 해당 리스트에서 빼주고
-            list_value.remove(index_value)
-            # 새로운 리스트에 추가한다.
-            new_list.append(index_value)
-    return new_list
+# sort 함수 정의
+def sort(list_value):
+    # 비교할 요소 정하기, 마지막에는 결국 제일 큰값이 올 것이기 때문에 마지막 index에서는 반복하지 않는다.
+    for index in range(len(list_value)-1):
+        # 비교할 요소 다음 index의 요소들 비교하기
+        for compare_index in range(index+1,len(list_value)):
+            # 만약 자기보다 작은 값이 나온다면
+            if list_value[index] > list_value[compare_index]:
+                # 자리 바꿔주기
+                compare_value = list_value[compare_index] # 변수에 작은 값을 담아준다.
+                list_value[compare_index] = list_value[index] # 작은 값이 있던 index의 요소를 큰 값으로 바꿔준다.
+                list_value[index] = compare_value # 큰 값이 있던 index의 요소를 작은 값으로 바꿔준다.
+    return list_value
 
 #  1 ~ 20 사이 양의 정수 중 난수 값 20개 생성 후 List에 저장
-List = [r.randint(1,20) for element in range(20)]
+List = [random.randint(1,20) for element in range(20)]
 
 # 요소 값 입력
 print("랜덤 값 :")
@@ -29,13 +32,12 @@ for index in range(len(List)):
         print(List[index],end=" ")
 
 # 작은 값부터 정렬하기
-List = sort(List, 0, 20)
+List = sort(List)
 
-#  List 내 원소 값에 대한 합계, 평균, 최대 값, 최소 값, 히스토그램 값 구하기
+#  List 내 원소 값에 대한 합계, 평균, 최대 값, 최소 값, 히스토그램 중복 값 구하기
 sum = 0
-minimum_value = List[0]
-maximum = List[-1]
 histogram_value = [0,0,0,0]
+overlap_value = {}
 for element in List:
     # List 안의 요소들을 더해준다
     sum += element
@@ -48,30 +50,26 @@ for element in List:
         histogram_value[2] += 1
     else:
         histogram_value[3] += 1
+    # 중복 값 정하기
+    if element not in overlap_value:
+        overlap_value[element] = 1
+    else:
+        overlap_value[element] += 1
+
 # 평균 --> 합계를 원소 갯수로 나누기
 avg = sum / len(List)
 
 # 합계, 평균, 최대 값, 최소 값 출력 하기
-print("최소 값 :", minimum_value)
-print("최대 값 :", maximum)
+print("최소 값 :", List[0])
+print("최대 값 :", List[-1])
 print("합계 \t:", sum)
 print("평균 \t:", avg)
 
+# 중복 값 출력하기
 print("중복 값","    ","중복 횟수")
-for element in List:
-# 중복 값과 중복 횟수 정하기
-    element_count = 0
-    # 어떤 값이 중복 되는지 정하기
-    for count in List:
-        # 얼마나 중복되는지 정하기
-        if element == count:
-            element_count += 1
-    if element_count >= 2:
-        # 중복된 값들을 빼주기 -- > 중복 값을 출력하는 부분에 여러 번 나올 수도 있다.
-        while element in List:
-            List.remove(element)
-        # 중복됐다면 값을 출력
-        print(" ",element,"\t\t",element_count)
+for key in overlap_value.keys():
+    if overlap_value[key] >= 2:
+        print(" ",key,"\t\t",overlap_value[key])
 
 #  구간 별 히스토그램 정보 출력
 print("구간별 히스토그램")
