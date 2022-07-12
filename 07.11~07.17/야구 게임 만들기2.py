ee9043c1db031c958ada55d7038e1672392ca271
Z00One <102473964+Z00One@users.noreply.github.com>
2022-07-12 20:20:31 +0900

    Update 야구 게임 만들기2.py

--- 07.11~07.17/야구 게임 만들기2.py
+++ 07.11~07.17/야구 게임 만들기2.py
@@ -5,10 +5,10 @@ answer_list     =    []
 # 요소가 중복되지 않는, 1에서 9까지의 범위로 난수 만들어주기
 # 정답 리스트에는 원소가 3가지
 while len(answer_list) < 3 :
-    answerValue       =    random.randint(1,9)
+    answerValue       =    str(random.randint(1,9))
     # 정답 리스트 안에 값이 없는 경우만 넣어 주기
     if answerValue not in answer_list:
-        answer_list.append(str(answerValue))
+        answer_list.append(answerValue)
 
 
 # 게임 안에서 계속 유지되어야 하는 변수 선언
@@ -39,48 +39,57 @@ while flag:
     
     
     # 값을 입력받기
-    inputValue          = input("정수 세 개를 입력하세요.")
+    inputValue          = input("정수 세 개를 입력하세요.\n")
     # 입력 받은 값의 리스트 만들어 주기
     inputValue_list     = [inputValue[0], inputValue[1], inputValue[2]]
 
-
+    input_index         = 0
     # 입력 받은 값을 정답과 비교해주기
-    for compareValue in range(len(inputValue_list)):
+    for compareValue in inputValue_list:
         # 요소값이 정답 리스트에 있는 지 확인
-        if inputValue_list[compareValue] in answer_list:
+        if compareValue in answer_list:
             # 인덱스 값도 맞다면 Strike
-            if inputValue_list[compareValue] == answer_list[compareValue]:
+            if compareValue == answer_list[input_index]:
                 Strike_count    +=  1
             # 인덱스 값이 다르면 Ball
             else :
                 Ball_count      +=  1
+        input_index += 1
 
 
     # 값을 출력하기
 
-        # 공백 출력
-        print()
+    # 공백 출력
+    print()
 
         # Strike값 Ball값 출력해주기
-        if Strike_count:
+    if Strike_count:
             print(Strike_count,"Strike",end=" ")
-        if Ball_count:
+    if Ball_count:
             print(Ball_count, "Ball", end=" ")
         # 겹치는 값, 인덱스가 없는 경우 --> Strike값 Ball값이 모두 0이라면 아웃
-        if Strike_count == 0 and Ball_count == 0:
+    if Strike_count == 0 and Ball_count == 0:
             out_count   +=  1
             print("Out : 아웃 %d번"%out_count)            
         
         # 공백 출력
-        print()
+    print()
 
     
     # 시도횟수가 5라면 종료
-    if try_count    >  5 :
+    if try_count   >=  5    or   out_count   >=  2:
         # 종료 안내문 출력
-        print("게임횟수 초과")
-        print("")
-        print("게임횟수 초과")
-    # 2아웃이면 종료
+        if try_count   >=  5:
+            print("게임횟수 초과")
+        else:
+            print("아웃카운트 초과")
+        print("아까비 졌네용..")
+        print("정답은:",answer_list[0],answer_list[1],answer_list[2],"입니다.")
+        flag = False
 
+    # 정답일 경우
+    # 3 Strike 일 경우
+    if Strike_count    ==   3 :
         # 종료 안내문 출력
+        print("정답입니다.")
+        flag = False 
