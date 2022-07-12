45887b4546e1bcc648d0f9e257ea3860579ea86e
Z00One <102473964+Z00One@users.noreply.github.com>
2022-07-12 20:20:27 +0900

    Update input.py

--- 07.11~07.17/input.py
+++ 07.11~07.17/input.py
@@ -1,2 +1,6 @@
-inputValue = input("정수 3개를 입력하세요 \n", end="")
+# inputValue = input("정수 3개를 입력하세요 \n", end="")
+# print(inputValue)
+
+inputValue  =   [int(input("입력해요")) for value in range(3)]
+
 print(inputValue)
