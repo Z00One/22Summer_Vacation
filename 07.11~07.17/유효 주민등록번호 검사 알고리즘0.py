14967c3a26942cb1a02190c63f68ee70904f789a
Z00One <102473964+Z00One@users.noreply.github.com>
2022-07-13 20:00:49 +0900

    Create 유효 주민등록번호 검사 알고리즘0.py

---
+++ 07.11~07.17/유효 주민등록번호 검사 알고리즘0.py
@@ -0,0 +1,45 @@
+sum        =  0     # a 값을 더해줄 변수 sum 선언
+checkValue =  2     # 체크값 선언
+
+# 주민등록번호를 입력받기
+# myNum = input("주민번호를 입력하세요")
+myNum = "990227-1676816"
+# 입력값을 리스트로 for 문을 돌리기 --> 마지막 요소가 아닌 숫자만 연산 해주기 --> 리스트 조정, 숫자 판별 필요
+for index in range(len(myNum)-1):
+    
+    # 숫자라면 진행
+    if myNum[index].isdigit():
+        pass
+    # 아니라면 다음 반복
+    else :
+        continue
+
+    # sum에 a 값을 추가
+    # 입력한 주민등록번호의 각 자리 마다 체크수를 곱해주기  --a             
+    sum += int(myNum[index]) * checkValue
+
+    # 체크 수는 반복 마다 바뀌게 하기
+    checkValue += 1
+
+    # 체크수는 10 되면 다시 2로 바뀐다.
+    if checkValue == 10:
+        checkValue = 2
+
+## 11 - (sum % 11) 값이 마지막 자리 값과 같은 경우
+## 11 - (sum % 11) 값이 두 자릿수가 나온다면 10으로 나눈 나머지 값과 마지막 자리값이 같은 경우
+judgeValue = 11 - (sum % 11)
+
+# ############################### 교수님께서 피드백 주신 부분 
+if judgeValue >= 10:
+    # judgeValue %= 10 도 쓸 수 있고 judgeValue -= 10 도 쓸 수 있음
+    judgeValue -= 10
+
+
+if judgeValue == int(myNum[-1]) or judgeValue % 10 == int(myNum[-1]):
+    # "유효한 주민번호 입니다." 출력
+    print("유효한 주민번호 입니다.")
+
+## 아니라면
+else :
+    # "유효하지 않은 주민번호 입니다." 출력
+    print("유효하지 않은 주민번호 입니다.")
