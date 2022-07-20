db97d94228b54e00d1abfedd789e26b034a73b2f
Z00One <102473964+Z00One@users.noreply.github.com>
2022-07-20 16:36:33 +0900

    Update 연습장.py

--- 07.18~07.24/연습장.py
+++ 07.18~07.24/연습장.py
@@ -1,2 +1,11 @@
-a = " "
-if not(a.isalpha()):print(1)
+# a = " "
+# if not(a.isalpha()):print(1)
+import random
+randomValues = [0 for value in range(5) ]
+
+# 난수를 발생시켜 하나의 리스트에 담는다.
+while len(randomValues) < 25:
+    randomValue = random.randint(1,30)
+    randomValues.append(randomValue) if randomValue not in randomValues else False
+
+print(randomValues)
