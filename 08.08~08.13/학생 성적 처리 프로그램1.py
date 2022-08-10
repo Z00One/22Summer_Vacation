0e0e270676f1edda66b0f44025b310f384e8abed
Z00One <102473964+Z00One@users.noreply.github.com>
2022-08-10 20:48:54 +0900

    Update 학생 성적 처리 프로그램1.py

--- 08.08~08.13/학생 성적 처리 프로그램1.py
+++ 08.08~08.13/학생 성적 처리 프로그램1.py
@@ -1,17 +1,29 @@
-inputValuelist   = []         # 입력데이터 담아줄 리스트
-sumOfAvgs        = 0          # 평균값들의 합, 평균값들의 평균
+# 학생 정보를 반환하는 함수 정의
+def getinputValue(**informationOfStudent):
+    inputValue = ''                                         # 학생 정보를 담을 변수 선언
+    sum        = 0                                          # 학생 점수 합계를 담을 변수 선언
+    
+    for key,value in informationOfStudent.items():          # 학생 정보 담기
+        inputValue += ' %s : '%key + str(value)
+        
+        if key == 'kor' or key == 'eng' or key == 'math' :  # 점수가 나오면 sum에 더해주기
+            sum += value
+    avg = sum / 3
+    
+    return avg, [inputValue + " sum : " + str(sum) + " avg : " + str(avg)]
 
-def getAvgValues(list):
-    avg = sumOfAvgs/len(list) if len(list) != 0 else 0.0
+# 값을 입력받는 함수 정의
+def getValue(value):
+    return input(value + "을 입력하세요\n")
 
-def getinputValue(*informationOfStudent):
-    inputValue = ''
-    for value in informationOfStudent:
-        inputValue += ' informationOfStudent : ' + str(informationOfStudent)
-    inputValuelist.append([inputValue])
+inputValuelist   = []                                       # 입력데이터 담아줄 리스트
+sumOfAvgs        = 0                                        # 평균값들의 합, 평균값들의 평균
+
 while True:
+    # 전체 평균 점수
     avgOfAvgs  = \
         sumOfAvgs/len(inputValuelist) if len(inputValuelist) != 0 else 0.0
+        
     # 기본 출력 메시지
     print("============================")
     print(" 1. 학생 성적 입력")
@@ -23,17 +35,13 @@ while True:
 
     # 선택
     selectValue = input("")
+    
     # 1 학생 성적 입력
     if selectValue == '1':
-        id   = input("학번을 입력하세요\n")
-        name = input("이름을 입력하세요\n")
-        kor  = int(input("국어 성적을 입력하세요\n"))
-        eng  = int(input("영어 성적을 입력하세요\n"))
-        math = int(input("수학 성적을 입력하세요\n"))
-        sum  = kor + eng + math
-        avg  = sum / 3
+        avg, inputValue =\
+            getinputValue(id = getValue("학번"), name = getValue("이름"), kor = int(getValue("국어점수")), eng = int(getValue("영어점수")), math = int(getValue("수학점수")))
 
-        inputValuelist.append(['id : ' + id + ' name : ' + name + ' kor : ' + str(kor) + ' eng : ' + str(eng) + ' math : ' + str(math) + ' sum : ' + str(sum) + ' avg : ' + str(avg)])
+        inputValuelist.append(inputValue)
         sumOfAvgs += avg
 
     # 2 학생 목록 출력
