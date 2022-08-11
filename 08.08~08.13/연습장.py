48d752e0d675f5272fa0ebc67ab1629e009a50de
Z00One <102473964+Z00One@users.noreply.github.com>
2022-08-11 20:51:08 +0900

    Update 연습장.py

--- 08.08~08.13/연습장.py
+++ 08.08~08.13/연습장.py
@@ -6,26 +6,26 @@
 # def getAvgValues(list):
 #     avg = sumOfAvgs/len(list) if len(list) != 0 else 0.0
 
-inputValuelist   = []         # 입력데이터 담아줄 리스트
-sumOfAvgs        = 0          # 평균값들의 합, 평균값들의 평균
+# inputValuelist   = []         # 입력데이터 담아줄 리스트
+# sumOfAvgs        = 0          # 평균값들의 합, 평균값들의 평균
 
-def appendinputValue(**informationOfStudent):
-    inputValue = ''
-    sum        = 0
-    for key,value in informationOfStudent.items():
-        inputValue += ' %s : '%key + str(value)
-        if key == 'kor' or key == 'eng' or key == 'math' :
-            sum += value
-    avg = sum / 3
-    return avg, [inputValue + " sum :" + str(sum) + " avg :" + str(avg)]
+# def appendinputValue(**informationOfStudent):
+#     inputValue = ''
+#     sum        = 0
+#     for key,value in informationOfStudent.items():
+#         inputValue += ' %s : '%key + str(value)
+#         if key == 'kor' or key == 'eng' or key == 'math' :
+#             sum += value
+#     avg = sum / 3
+#     return avg, [inputValue + " sum :" + str(sum) + " avg :" + str(avg)]
     
-def getValue(value):    
-    return input(value + "을 입력하세요\n")
+# def getValue(value):    
+#     return input(value + "을 입력하세요\n")
 
     
 
-sum, avg = \
-    appendinputValue(id = getValue("학번"), name = getValue("이름"), kor = int(getValue("국어점수")), eng = int(getValue("영어점수")), math = int(getValue("수학점수")))
+# sum, avg = \
+#     appendinputValue(id = getValue("학번"), name = getValue("이름"), kor = int(getValue("국어점수")), eng = int(getValue("영어점수")), math = int(getValue("수학점수")))
 
 
-print(inputValuelist)
+# print(inputValuelist)
