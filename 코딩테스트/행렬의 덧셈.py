be00a23458ab9e9b105002e641486b5e08a30628
Z00One <102473964+Z00One@users.noreply.github.com>
2022-08-22 19:10:14 +0900

    Delete 행렬의 덧셈.py

--- 코딩테스트/행렬의 덧셈.py
+++
@@ -1,9 +0,0 @@
-def solution(arr1, arr2):
-    
-    for element in range(len(arr1)): # 더해줄 요소 정하기
-        for index in range(len(arr1[element])): # 더해줄 요소의 인덱스 정하기
-            arr1[element][index] += arr2[element][index]
-            
-    return arr1
-
-print(solution([[1,2],[2,3]],[[3,4],[5,6]]))
