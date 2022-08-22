60aff465a4b8464f4bcf675a843162505a097379
Z00One <102473964+Z00One@users.noreply.github.com>
2022-08-22 19:10:07 +0900

    Create 기능개발.py

---
+++ 코딩테스트/프로그래머스/level2/기능개발.py
@@ -0,0 +1,43 @@
+def solution(progresses, speeds):
+    answer = []
+
+    # while progresses:
+    #     count = 0
+    #     dayValue = (100 - progresses[0]) // speeds[0]
+        
+    #     for index in range(len(progresses)):
+    #         if (progresses[index] + (dayValue * speeds[index])) < 100:
+    #             break
+    #         count += 1
+        
+    #     progresses = progresses[index:] if len(progresses) > 2 else []
+    #     speeds     = speeds[index:]
+                
+    #     answer.append(count)
+    count = 0
+    
+    for index in range(len(progresses)):
+        if not(count) :
+            dayValue = (100 - progresses[index]) // speeds[index]
+
+        if (progresses[index] + (dayValue * speeds[index])) < 100:
+            answer.append(count)
+            dayValue = (100 - progresses[index]) // speeds[index]
+            count = 0
+        
+        count +=1
+
+    answer.append(count)            
+
+    return answer
+print(solution([96, 99, 98, 97], [1,1,1,1]))
+
+# 예제 1)[99, 99, 99] ,[1, 1,1]
+# progresses : [40, 93, 30, 55, 60, 65]
+# speeds : [60, 1, 30, 5 , 10, 7]
+# return : [1,2,3]
+# [99, 1, 99, 1] [1, 1, 1, 1] [1, 3]
+# 예제 2)
+# progresses : [93, 30, 55, 60, 40, 65]
+# speeds : [1, 30, 5 , 10, 60, 7]
+# return : [2,4]
