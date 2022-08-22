4accabd846afb9cee08b522970182ae555bfc0e8
Z00One <102473964+Z00One@users.noreply.github.com>
2022-08-22 19:07:42 +0900

    Delete 제일 작은 수 제거하기.py

--- 코딩테스트/제일 작은 수 제거하기.py
+++
@@ -1,8 +0,0 @@
-def solution(arr):
-    if arr == [10] : return [-1]
-    min = arr[0]
-    for element in arr:
-        if element < min: min = element
-    arr.remove(min)
-    return arr
-print(solution([4, 3, 2, 1]))
