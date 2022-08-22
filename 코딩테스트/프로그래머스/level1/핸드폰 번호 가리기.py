9a613a1e993a6888953771a922ac724d63cddcf5
Z00One <102473964+Z00One@users.noreply.github.com>
2022-08-22 19:10:01 +0900

    Create 핸드폰 번호 가리기.py

---
+++ 코딩테스트/프로그래머스/level1/핸드폰 번호 가리기.py
@@ -0,0 +1,5 @@
+# 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수
+def solution(phone_number):
+    # 전화번호 뒷자리 4자리를 제외한 나머지는 "*"로 대체한다
+    return "*"*(len(phone_number)-4)   +   phone_number[-4:]
+print(solution(""))
