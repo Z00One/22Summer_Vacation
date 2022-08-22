23122015f6f9fe3db5836b88c06447c253c70944
Z00One <102473964+Z00One@users.noreply.github.com>
2022-08-22 19:07:53 +0900

    Delete 폰켓몬.py

--- 코딩테스트/폰켓몬.py
+++
@@ -1,6 +0,0 @@
-def solution(nums):
-    newNums    = len(set(nums))         # 중복 제거
-    count      = len(nums) // 2         # 고를 수 있는 폰켓몬 수
-
-    answer = count if count < newNums else newNums
-    return answer
