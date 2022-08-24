b2d9879246102fa85cc88077f3b4fe52b3f9e4f4
Z00One <102473964+Z00One@users.noreply.github.com>
2022-08-24 10:34:28 +0900

    Update 기능개발0.py

--- 코딩테스트/프로그래머스/level2/기능개발0.py
+++ 코딩테스트/프로그래머스/level2/기능개발0.py
@@ -1,56 +1,48 @@
-# def solution(progresses, speeds):
-#     answer = []
+def solution(progresses, speeds):
+    answer = []
 
-#     while progresses:
+    while progresses:
         
-#         dayValue = (100 - progresses[0]) // speeds[0]
+        dayValue = (100 - progresses[0]) // speeds[0]
         
-#         progresses = [progresses[index] + dayValue * speeds[index] for index in range(len(progresses))]
+        progresses = [progresses[index] + dayValue * speeds[index] for index in range(len(progresses))]
         
-#         count = 0
+        count = 0
         
-#         for index in range(len(progresses)):
-#             if progresses[index] < 100:
-#                 break
-#             count +=1
+        for index in range(len(progresses)):
+            if progresses[index] < 100:
+                break
+            count +=1
 
-#         progresses = progresses[index :] if len(progresses) > 2 else 0
-#         speeds     = speeds[index :]
+        progresses = progresses[index :] if len(progresses) > 2 else 0
+        speeds     = speeds[index :]
 
-#         if count:
-#             answer.append(count)            
+        if count:
+            answer.append(count)            
 
-#     return answer
-
-#####
-# def solution(progresses, speeds):
-#     answer = []
-        
-#     dayValue = (100 - progresses[0]) // speeds[0]
-
-#     progresses = [progresses[index] + dayValue * speeds[index] for index in range(len(progresses))]
-
-#     count = 0
-
-#     for index in range(len(progresses)):
-#         if progresses[index] < 100:
-#             break
-#         count +=1
-
-#     if count:
-#         answer.append(count)
-#         if count == len(progresses):
-#             return answer
-#         return answer + solution(progresses[index :], speeds[index :])
-
+    return answer
 
 ####
 def solution(progresses, speeds):
     answer = []
+        
+    dayValue = (100 - progresses[0]) // speeds[0]
 
-    progresses = [(100 - progresses[index]) // speeds[index] for index in range(len(progresses))]
+    progresses = [progresses[index] + dayValue * speeds[index] for index in range(len(progresses))]
 
-    for index in 
+    count = 0
 
-    return answer
+    for index in range(len(progresses)):
+        if progresses[index] < 100:
+            break
+        count +=1
+
+    if count:
+        answer.append(count)
+        if count == len(progresses):
+            return answer
+        return answer + solution(progresses[index :], speeds[index :])
+
+
+
 print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
